b5c88122c450826b5f8c5874a25ebff7f89e244c
Mateus Melo <mateusmeloxyz@gmail.com>
2024-04-18 00:17:55 -0300

    Create basic todo app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
 
+class Todo {
+  final String title;
+  final String description;
+
+  const Todo(this.title, this.description);
+}
+
+final todos = List.generate(
+  20,
+  (i) => Todo(
+    'Todo $i',
+    'A description of what needs to be done for Todo $i',
+  ),
+);
+
 void main() {
-  runApp(const MainApp());
+  runApp(
+    MaterialApp(
+      title: 'Passing Data',
+      home: TodosScreen(
+        todos: todos
+      )
+    )
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -14,6 +36,64 @@ class MainApp extends StatelessWidget {
         body: Center(
           child: Text('Hello World!'),
         ),
+      ),
+    );
+  }
+}
+
+class TodosScreen extends StatelessWidget {
+  // Requiring the list of todos.
+  const TodosScreen({super.key, required this.todos});
+
+  final List<Todo> todos;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todos'),
+      ),
+      //passing in the ListView.builder
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(todos[index].title),
+            // When a user taps the ListTile, navigate to the DeailScreen.
+            // Notice that you're not only creating a DetailScreen, you're
+            // also passing the current todo through to it.
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailScreen(todo: todos[index]),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  // In the constructor, require a Todo
+  const DetailScreen({super.key, required this.todo});
+
+  // Declare a field that holds the Todo.
+  final Todo todo;
+
+  @override
+  Widget build(BuildContext context) {
+    // Use the Todo to create the UI.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(todo.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Text(todo.description),
       ),
     );
   }
